b3b1b2cb19c7a66e4da8cada9cd3cb9f5f1d9862
Sourabh Dharpure <35331618+sourabhdharpure@users.noreply.github.com>
2023-06-17 15:56:41 +0530

    response code

--- src/user/router.js
+++ src/user/router.js
@@ -7,7 +7,7 @@ const router = require('express').Router();
 router.post('/', async (req, res) => {
     return await userFactory.saveContact(req, res)
         .then((resp) => { res.send({...resp }) })
-        .catch((e) => { res.status(500).send({status:'error', msg: 'user not found'}) })
+        .catch((e) => { res.status(404).send({status:'error', msg: 'user not found'}) })
 })
 
 module.exports = router
